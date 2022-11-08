3c24d3536f39fc46c9afd0e6956b5577042e863c
Mihail <mohonovproduction@yandex.ru>
2022-11-09 00:34:55 +0300

    font

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,20 @@
 
 <style>
 
+@font-face {
+  font-family: 'Programme';
+  src: local('Programme Regular'), local('Programme-Regular'),
+  url('@/assets/fonts/Programme-Regular.woff2') format('woff2'),
+  url('@/assets/fonts/Programme-Regular.woff') format('woff'),
+  url('@/assets/fonts/Programme-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-style: normal;
+}
+
 :root {
   box-sizing: content-box;
+  font-family: Programme, Arial, sans-serif;
+  font-feature-settings: "ss07", "ss08", "ss11", "ss12", "ss14", "ss15", "ss16", "ss18", "ss19", "ss20", "ss21";
 }
 
 *,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <h1>HHHHHH VIERFIED &</h1>
 </template>
 
 <script>
