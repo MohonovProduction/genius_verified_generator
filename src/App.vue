20f84f30525375dc840a62314aa98fd9287a8959
Mihail <mohonovproduction@yandex.ru>
2022-11-09 00:23:10 +0300

    add music

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+:root {
+  box-sizing: content-box;
 }
 
-nav {
-  padding: 30px;
+*,
+:before,
+:after {
+  padding: 0;
+  border: none;
+  margin: 0;
+  outline: none;
+  background: none;
+  box-sizing: inherit;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
